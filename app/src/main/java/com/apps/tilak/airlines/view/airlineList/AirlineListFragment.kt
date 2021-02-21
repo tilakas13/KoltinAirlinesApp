cc3ed92d778f71978e97f2cc1e68531ce27a3d52
tilak <tilakas13@gmail.com>
2021-02-21 13:24:05 +0530

    Progress indicator Added added

--- app/src/main/java/com/apps/tilak/airlines/view/airlineList/AirlineListFragment.kt
+++ app/src/main/java/com/apps/tilak/airlines/view/airlineList/AirlineListFragment.kt
@@ -4,13 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-
 import com.apps.tilak.airlines.ViewModelFactory
 import com.apps.tilak.airlines.model.AirlineItem
 import com.apps.tilak.airlines.network.ApiHelper
@@ -23,6 +25,8 @@ class AirlineListFragment : Fragment() {
     companion object {
         fun newInstance() = AirlineListFragment()
     }
+
+    private lateinit var progressBarLoading: ProgressBar;
 
     private lateinit var adapterAirlines: AirlinesListAdapter
     private lateinit var viewModel: AirlineListViewModel
@@ -37,47 +41,39 @@ class AirlineListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val recyclerView: RecyclerView = view.findViewById(R.id.recyclerview_airlines)
+        progressBarLoading = view.findViewById(R.id.progressBar);
         adapterAirlines = AirlinesListAdapter(airlinesList)
         val layoutManager = LinearLayoutManager(activity)
         recyclerView.layoutManager = layoutManager
         recyclerView.adapter = adapterAirlines
-        prepareAirlinesListItems()
     }
 
-    /**
-     * Creating dummy items for listing
-     */
-
-    private fun prepareAirlinesListItems() {
-        for (i in 1..100) {
-            var airlineItem = AirlineItem()
-            airlineItem.defaultName = "Mad Max: Fury Road $i"
-            airlinesList.add(airlineItem)
-        }
-    }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         (requireActivity() as AppCompatActivity).supportActionBar?.show()
-        // viewModel = ViewModelProvider(this).get(AirlineListViewModel::class.java)
         viewModel = ViewModelProviders.of(
             this,
             ViewModelFactory(ApiHelper(RetrofitBuilder.apiService))
         ).get(AirlineListViewModel::class.java)
+        progressBarLoading.visibility = View.VISIBLE
         viewModel.getListAirlines().observe(viewLifecycleOwner, Observer {
             it?.let { resource ->
                 when (resource.status) {
                     Status.SUCCESS -> {
+                        progressBarLoading.visibility = View.GONE
                         //recyclerView.visibility = View.VISIBLE
                         // progressBar.visibility = View.GONE
                         resource.data?.let { users -> retrieveList(users) }
                     }
                     Status.ERROR -> {
+                      //  progressBarLoading.isVisible = false
                         //recyclerView.visibility = View.VISIBLE
                         // progressBar.visibility = View.GONE
                         // Toast.makeText(this, it.message, Toast.LENGTH_LONG).show()
                     }
                     Status.LOADING -> {
+                        progressBarLoading.isVisible = false
                         // progressBar.visibility = View.VISIBLE
                         // recyclerView.visibility = View.GONE
                     }

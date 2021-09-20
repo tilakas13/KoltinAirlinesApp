a9deeb62251c6539b3ed009cdd69f3e784d962c6
tilak <tilakas13@gmail.com>
2021-09-20 19:54:10 +0530

    Updated dependencies

--- app/src/main/java/com/apps/tilak/airlines/presentation/ui/listAirlines/AirlineListViewModel.kt
+++ app/src/main/java/com/apps/tilak/airlines/presentation/ui/listAirlines/AirlineListViewModel.kt
@@ -2,14 +2,12 @@ package com.apps.tilak.airlines.presentation.ui.listAirlines
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.liveData
 import androidx.lifecycle.viewModelScope
 import com.apps.tilak.airlines.base.BaseViewModel
 import com.apps.tilak.airlines.data.model.AirlineItem
 import com.apps.tilak.airlines.data.repository.AirlinesRepository
 import com.apps.tilak.airlines.presentation.ui.utils.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -18,30 +16,17 @@ class AirlineListViewModel
 @Inject
 constructor(private val airlinesRepository: AirlinesRepository) : BaseViewModel() {
 
-    private val _res = MutableLiveData<Resource<List<AirlineItem>>>()
+    private val listAirlines = MutableLiveData<Resource<List<AirlineItem>>>()
     val res : LiveData<Resource<List<AirlineItem>>>
-        get() = _res
+        get() = listAirlines
 
-   /* fun setRepository(airlinesRepository: AirlinesRepository) {
-        this.airlineRepository = airlinesRepository
-    }*/
-
-//    fun getListAirlines() = liveData(Dispatchers.IO) {
-//        emit(Resource.loading(data = null))
-//        try {
-//            emit(Resource.success(data = airlinesRepository.getListAirlines()))
-//        } catch (exception: Exception) {
-//            emit(Resource.error(data = null, message = exception.message ?: "Error Occurred!"))
-//        }
-//    }
-
-    public fun getListAirlines()  = viewModelScope.launch {
-        _res.postValue(Resource.loading(null))
+    fun getListAirlines()  = viewModelScope.launch {
+        listAirlines.postValue(Resource.loading(null))
         airlinesRepository.getListAirlines().let {
             if (it.isSuccessful){
-                _res.postValue(Resource.success(it.body()))
+                listAirlines.postValue(Resource.success(it.body()))
             }else{
-                _res.postValue(Resource.error(it.errorBody().toString(), null))
+                listAirlines.postValue(Resource.error(it.errorBody().toString(), null))
             }
         }
     }
